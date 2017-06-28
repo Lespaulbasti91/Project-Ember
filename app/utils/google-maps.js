a4e3b7a9beaaaf27ac271642c34dff1d9496ac4b
Lespaulbasti <Sebastian.Schieck91@gmail.com>
2017-06-28 18:54:40 +0200

    added my position in google maps

--- app/utils/google-maps.js
+++ app/utils/google-maps.js
@@ -4,23 +4,32 @@ const google = window.google;
 
 export default Ember.Object.extend({
 
+
   init() {
     this.set('geocoder', new google.maps.Geocoder());
   },
 
   createMap(element, location) {
-    let map = new google.maps.Map(element, { scrollwheel: false, zoom: 10 });
+    let map = new google.maps.Map(element, { scrollwheel: true, zoom: 13 });
     this.pinLocation(location, map);
     return map;
   },
 
+
+
   pinLocation(location, map) {
     this.get('geocoder').geocode({address: location}, (result, status) => {
       if (status === google.maps.GeocoderStatus.OK) {
-        let geometry = result[0].geometry.location;
-        let position = { lat: geometry.lat(), lng: geometry.lng() };
-        map.setCenter(position);
-        new google.maps.Marker({ position, map, title: location });
+        navigator.geolocation.getCurrentPosition(function(myPosition) {
+          let pos = {
+            lat: myPosition.coords.latitude,
+            lng: myPosition.coords.longitude
+          };
+          //let geometry = result[0].geometry.location;
+          let position = pos;
+          map.setCenter(position);
+          new google.maps.Marker({ position, map, title: location });
+        });
       }
     });
   }
